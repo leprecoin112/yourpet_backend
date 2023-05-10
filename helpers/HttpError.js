92f9067ae0417f5892988593613aa6f2b9c21aea
Yaroslav Khakhuda <hahyda14@gmail.com>
2023-05-10 19:44:42 +0300

    initial project

---
+++ helpers/HttpError.js
@@ -0,0 +1,27 @@
+module.exports = class HttpError extends Error {
+  status;
+
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+  }
+
+  static UnauthorizedError(message = "Not authorized") {
+    return new HttpError(401, message);
+  }
+
+  static BadRequest(message = "Bad Request") {
+    return new HttpError(400, message);
+  }
+
+  static ForbiddenError() {
+    return new HttpError(403, "Forbidden.");
+  }
+
+  static NotFoundError(message = "Not Found.") {
+    return new HttpError(404, message);
+  }
+  static ConflictError(message = "Conflict.") {
+    return new HttpError(409, message);
+  }
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const PORT = process.env.PORT || 5001;
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_HOST);
+    console.log("Database connection successful");
+    app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
