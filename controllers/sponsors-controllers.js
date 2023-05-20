8a6d145f4b99955bd0a36f6fcea770652d319c66
Yaroslav Khakhuda <hahyda14@gmail.com>
2023-05-20 12:57:59 +0300

    fix sponsors controllers

--- controllers/sponsors-controllers.js
+++ controllers/sponsors-controllers.js
@@ -1,22 +1,19 @@
 const ctrlWrapper = require("../utils/ctrlWrapper");
-const { News } = require("../models/news");
-const HttpError = require('../helpers/HttpError');
+const { Sponsor } = require("../models/sponsors");
+const HttpError = require("../helpers/HttpError");
 
 const getAllSponsors = async (req, res) => {
-   
-    const result = await News.find();
+  const result = await Sponsor.find();
 
-      if(result.length === 0) {
-        throw HttpError.NotFoundError("Sponsors not found");
-      }
-
-
-    res.status(200).json({
-        result,
-      },
-    );
-  };
-
-  module.exports = {
-    getAllSponsors: ctrlWrapper(getAllSponsors),
+  if (result.length === 0) {
+    throw HttpError.NotFoundError("Sponsors not found");
   }
+
+  res.status(200).json({
+    result,
+  });
+};
+
+module.exports = {
+  getAllSponsors: ctrlWrapper(getAllSponsors),
+};
