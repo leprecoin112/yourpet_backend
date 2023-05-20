399fb1d02928aad66ddc5c976a312d808b2e7ba2
Yaroslav Khakhuda <hahyda14@gmail.com>
2023-05-20 12:10:21 +0300

    add cors client url

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-const swaggerUi = require('swagger-ui-express');
+const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
 const authRouter = require("./routes/api/auth-routes");
-const newsRouter = require('./routes/api/news-routes');
-const sponsorsRouter = require('./routes/api/sponsors-routes');
+const newsRouter = require("./routes/api/news-routes");
+const sponsorsRouter = require("./routes/api/sponsors-routes");
 const noticeRouter = require("./routes/api/notices-routes");
 const userRouter = require("./routes/api/user-routes");
-const addPetsRouter = require("./routes/api/pets-routers")
+const addPetsRouter = require("./routes/api/pets-routers");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.CLIENT_URL, process.env.CLIENT_DEV_URL],
+  })
+);
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -27,7 +32,6 @@ app.use("/api/sponsors", sponsorsRouter);
 app.use("/api/notices", noticeRouter);
 app.use("/api/user", userRouter);
 app.use("/api/pets", addPetsRouter);
-
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
